e52870cddedfea0b53577d90878539894b81d56d
Alex Mitchell <amitchell@ctatax.uk.com>
2023-01-01 22:31:18 +0000

    Fetch cards from DB, render a Card component for each fetched card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import Card from "./components/Card";
+import { useState, useEffect } from "react";
+
 export default function App() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8002/api/cards/`)
+      .then((response) => response.json())
+      .then((data) => setCards(data));
+  });
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-} 
+    <>
+      <h1 className="text-3xl font-bold underline">Cards</h1>
+      {cards.map((card, index) => {
+        return (
+          <Card {...card} key={index}/>
+        );
+      })}
+    </>
+  );
+}
