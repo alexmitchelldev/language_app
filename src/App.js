ee007a365ad327cd1b0979c68e2efe81abd36fb2
Alex Mitchell <amitchell@ctatax.uk.com>
2023-01-02 14:24:01 +0000

    Remove unused imports, prettier file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import Header from "./components/Header";
 import Cards from "./pages/Cards";
-import Card from "./components/Card";
 import AddCard from "./components/AddCard";
-import { useState, useEffect } from "react";
 
 export default function App() {
-  
-
   return (
     <>
       <Header>
