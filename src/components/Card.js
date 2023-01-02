8f14560b30bcf372c81a2f4ace3b128b68771267
Alex Mitchell <amitchell@ctatax.uk.com>
2023-01-02 10:42:31 +0000

    Add coffee break french tag, prettier file

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,31 +5,37 @@ const Card = (props) => {
   let tagsArray = null;
 
   useEffect(() => {
-    fetch('http://localhost:8002/api/tags')
-    .then((response) => response.json())
-    .then((data) => setTagsStyles(data));
-  })
+    fetch("http://localhost:8002/api/tags")
+      .then((response) => response.json())
+      .then((data) => setTagsStyles(data));
+  });
 
   const tags = {
-    "french comprehensible input" : {
-        color: {
-            background: "bg-red-300",
-            text: "text-red-600"
-        }
+    "french comprehensible input": {
+      color: {
+        background: "bg-red-300",
+        text: "text-red-600",
+      },
     },
-    beginner : {
-        color: {
-            background: "bg-green-300",
-            text: "text-green-600"
-        }
+    beginner: {
+      color: {
+        background: "bg-green-300",
+        text: "text-green-600",
+      },
     },
     french: {
       color: {
         background: "bg-blue-300",
-        text: "text-blue-600"
+        text: "text-blue-600",
+      },
+    },
+    "coffee break french": {
+      color: {
+        background: "bg-yellow-600",
+        text: "text-yellow-800"
       }
     }
-}
+  };
 
   if (props.tags) {
     tagsArray = JSON.parse(props.tags);
@@ -37,7 +43,7 @@ const Card = (props) => {
 
   return (
     <>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="w-full md:w-1/3 lg:w-1/5 rounded overflow-hidden shadow-lg">
         {props.img ? (
           <img
             className="w-full"
@@ -52,10 +58,15 @@ const Card = (props) => {
         <div className="px-6 pt-4 pb-2">
           {props.tags
             ? tagsArray.map((tag) => {
-              let tagClass = "inline-block rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2";
-              const tagBackground = tags[`${tag}`] ? tags[`${tag}`].color.background : "bg-gray-200";
-              const tagText = tags[`${tag}`] ? tags[`${tag}`].color.text : "text-gray-700";
-              tagClass += ` ${tagBackground} ${tagText}`;
+                let tagClass =
+                  "inline-block rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2";
+                const tagBackground = tags[`${tag}`]
+                  ? tags[`${tag}`].color.background
+                  : "bg-gray-200";
+                const tagText = tags[`${tag}`]
+                  ? tags[`${tag}`].color.text
+                  : "text-gray-700";
+                tagClass += ` ${tagBackground} ${tagText}`;
                 return <span className={tagClass}>#{tag}</span>;
               })
             : null}
