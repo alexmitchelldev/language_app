e7b086737d1c2329424a82ba5d27de7e4ff5d3e3
Alex Mitchell <amitchell@ctatax.uk.com>
2023-01-02 21:39:57 +0000

    Add AddCard to Cards component,

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,21 +1,48 @@
 import Card from "../components/Card.js";
+import AddCard from "../components/AddCard.js";
 import { useState, useEffect } from "react";
 
 const Cards = () => {
   const [cards, setCards] = useState([]);
 
-  useEffect(() => {
-    fetch(`http://localhost:8002/api/cards/`)
-      .then((response) => response.json())
-      .then((data) => setCards(data));
-  });
+  const GetCards = () => {
+    useEffect(() => {
+      fetch(`http://localhost:8002/api/cards/`)
+        .then((response) => response.json())
+        .then((data) => setCards(data));
+    });
+  };
+
+  const addCard = (body) => {
+    const addCardBody = new URLSearchParams({
+      titleFront: body.titleFront,
+      titleBack: body.titleBack,
+      textFront: body.textFront,
+      tags: body.tags,
+      languageFront: body.languageFront,
+      languageBack: body.languageBack
+    });
+    console.log(addCardBody);
+    // https://stackoverflow.com/questions/71678250/how-to-post-body-data-using-fetch-api
+    fetch("http://localhost:8002/api/cards", {
+      method: "POST",
+      body: addCardBody
+    });
+    GetCards();
+  };
+
   return (
     <>
-      <div className="flex flex-wrap justify-center mx-auto lg:w-4/5" id="cards-area">
+      {GetCards()}
+      <div
+        className="flex flex-wrap justify-center mx-auto lg:w-4/5"
+        id="cards-area"
+      >
         {cards.map((card, index) => {
           return <Card {...card} key={index} />;
         })}
       </div>
+      <AddCard addCard={addCard} />
     </>
   );
 };
