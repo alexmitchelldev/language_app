e887433172623da7ed54e8acd708bdeba861d338
Alex Mitchell <amitchell@ctatax.uk.com>
2023-01-02 21:47:05 +0000

    Add my Gravatar to nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -84,7 +84,7 @@ export default function Header(props) {
                         <span className="sr-only">Open user menu</span>
                         <img
                           className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          src="https://en.gravatar.com/userimage/218231086/8c8277607c361c251e41db4b300773b1.png?size=100"
                           alt=""
                         />
                       </Menu.Button>
