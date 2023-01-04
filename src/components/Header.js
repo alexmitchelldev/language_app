acf8976efb47cd82cd9862811642bdf35f06325e
mitchting <amitchell@ctatax.uk.com>
2023-01-04 17:44:59 +0000

    Add no-underline class to navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -58,7 +58,7 @@ export default function Header(props) {
                             item.current
                               ? "bg-gray-900 text-white"
                               : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                            "px-3 py-2 rounded-md text-sm font-medium"
+                            "px-3 py-2 rounded-md text-sm font-medium no-underline"
                           )}
                           aria-current={item.current ? "page" : undefined}
                         >
